659a24ae500fea8b030adc4faf52a2274dd08094
Xand Lin <allex.santos135@gmail.com>
2021-02-21 19:46:45 -0300

    Testa C

---
+++ ProjeMedia/ProjeMedia/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace ProjeMedia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textNota1 = new System.Windows.Forms.TextBox();
+            this.textNota2 = new System.Windows.Forms.TextBox();
+            this.textNota3 = new System.Windows.Forms.TextBox();
+            this.textNota4 = new System.Windows.Forms.TextBox();
+            this.textMedia = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label1.Location = new System.Drawing.Point(117, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Média Do Aluno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label2.Location = new System.Drawing.Point(117, 143);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nota 1°";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label3.Location = new System.Drawing.Point(117, 178);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota 2°";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label4.Location = new System.Drawing.Point(117, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nota 3°";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label5.Location = new System.Drawing.Point(117, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nota 4°";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.GreenYellow;
+            this.label6.Location = new System.Drawing.Point(117, 358);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Média:";
+            // 
+            // textNota1
+            // 
+            this.textNota1.BackColor = System.Drawing.Color.Chartreuse;
+            this.textNota1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNota1.Location = new System.Drawing.Point(187, 142);
+            this.textNota1.Name = "textNota1";
+            this.textNota1.Size = new System.Drawing.Size(67, 26);
+            this.textNota1.TabIndex = 6;
+            // 
+            // textNota2
+            // 
+            this.textNota2.BackColor = System.Drawing.Color.Chartreuse;
+            this.textNota2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNota2.Location = new System.Drawing.Point(187, 177);
+            this.textNota2.Name = "textNota2";
+            this.textNota2.Size = new System.Drawing.Size(67, 26);
+            this.textNota2.TabIndex = 7;
+            // 
+            // textNota3
+            // 
+            this.textNota3.BackColor = System.Drawing.Color.Chartreuse;
+            this.textNota3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNota3.Location = new System.Drawing.Point(187, 211);
+            this.textNota3.Name = "textNota3";
+            this.textNota3.Size = new System.Drawing.Size(67, 26);
+            this.textNota3.TabIndex = 8;
+            // 
+            // textNota4
+            // 
+            this.textNota4.BackColor = System.Drawing.Color.Chartreuse;
+            this.textNota4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textNota4.Location = new System.Drawing.Point(187, 249);
+            this.textNota4.Name = "textNota4";
+            this.textNota4.Size = new System.Drawing.Size(67, 26);
+            this.textNota4.TabIndex = 9;
+            // 
+            // textMedia
+            // 
+            this.textMedia.BackColor = System.Drawing.Color.Chartreuse;
+            this.textMedia.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMedia.Location = new System.Drawing.Point(187, 357);
+            this.textMedia.Name = "textMedia";
+            this.textMedia.Size = new System.Drawing.Size(67, 26);
+            this.textMedia.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.GreenYellow;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.button1.Location = new System.Drawing.Point(142, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 27);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "CALCULAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.GreenYellow;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.button2.Location = new System.Drawing.Point(154, 314);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 27);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Limpa";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.GreenYellow;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Yellow;
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.button3.Location = new System.Drawing.Point(165, 413);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(47, 25);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Sair";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::ProjeMedia.Properties.Resources._8iAP1eI;
+            this.ClientSize = new System.Drawing.Size(379, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textMedia);
+            this.Controls.Add(this.textNota4);
+            this.Controls.Add(this.textNota3);
+            this.Controls.Add(this.textNota2);
+            this.Controls.Add(this.textNota1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textNota1;
+        private System.Windows.Forms.TextBox textNota2;
+        private System.Windows.Forms.TextBox textNota3;
+        private System.Windows.Forms.TextBox textNota4;
+        private System.Windows.Forms.TextBox textMedia;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ProjeMedia/ProjeMedia/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjeMedia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3, nota4, media;
+            nota1 = Convert.ToDouble(textNota1.Text);
+            nota2 = Convert.ToDouble(textNota2.Text);
+            nota3 = Convert.ToDouble(textNota3.Text);
+            nota4 = Convert.ToDouble(textNota4.Text);
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            textMedia.Text = media.ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textNota1.Text = "";
+            textNota2.Text = "";
+            textNota3.Text = "";
+            textNota4.Text = "";
+            textMedia.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        
+    }
+}
