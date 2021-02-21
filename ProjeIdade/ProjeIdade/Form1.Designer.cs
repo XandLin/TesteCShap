7568cbd1afb5cf825d2a9e2faca354e646ed4dae
Xand Lin <allex.santos135@gmail.com>
2021-02-21 19:52:18 -0300

    teste

---
+++ ProjeIdade/ProjeIdade/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace ProjeIdade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textDia = new System.Windows.Forms.TextBox();
+            this.textMes = new System.Windows.Forms.TextBox();
+            this.textAno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textHojeAno = new System.Windows.Forms.TextBox();
+            this.textHojeMes = new System.Windows.Forms.TextBox();
+            this.textHojeDia = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textResult = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textDia
+            // 
+            this.textDia.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textDia.Location = new System.Drawing.Point(142, 230);
+            this.textDia.Name = "textDia";
+            this.textDia.Size = new System.Drawing.Size(53, 20);
+            this.textDia.TabIndex = 0;
+            // 
+            // textMes
+            // 
+            this.textMes.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textMes.Location = new System.Drawing.Point(188, 230);
+            this.textMes.Name = "textMes";
+            this.textMes.Size = new System.Drawing.Size(54, 20);
+            this.textMes.TabIndex = 1;
+            // 
+            // textAno
+            // 
+            this.textAno.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textAno.Location = new System.Drawing.Point(236, 230);
+            this.textAno.Name = "textAno";
+            this.textAno.Size = new System.Drawing.Size(74, 20);
+            this.textAno.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(111, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(225, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Digite a Data De Nascimento";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(149, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 14);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Dia:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(197, 212);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 14);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mês:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(259, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 14);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ano:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(174, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 25);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Enviar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(259, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 14);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Ano:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(197, 86);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(34, 14);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Mês:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(149, 86);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 14);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Dia:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(170, 42);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(108, 19);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Data De Hoje";
+            // 
+            // textHojeAno
+            // 
+            this.textHojeAno.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textHojeAno.Location = new System.Drawing.Point(236, 104);
+            this.textHojeAno.Name = "textHojeAno";
+            this.textHojeAno.Size = new System.Drawing.Size(74, 20);
+            this.textHojeAno.TabIndex = 10;
+            // 
+            // textHojeMes
+            // 
+            this.textHojeMes.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textHojeMes.Location = new System.Drawing.Point(188, 104);
+            this.textHojeMes.Name = "textHojeMes";
+            this.textHojeMes.Size = new System.Drawing.Size(54, 20);
+            this.textHojeMes.TabIndex = 9;
+            // 
+            // textHojeDia
+            // 
+            this.textHojeDia.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textHojeDia.Location = new System.Drawing.Point(142, 104);
+            this.textHojeDia.Name = "textHojeDia";
+            this.textHojeDia.Size = new System.Drawing.Size(53, 20);
+            this.textHojeDia.TabIndex = 8;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(104, 330);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(71, 16);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Resultado";
+            // 
+            // textResult
+            // 
+            this.textResult.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textResult.Location = new System.Drawing.Point(181, 326);
+            this.textResult.Name = "textResult";
+            this.textResult.Size = new System.Drawing.Size(110, 20);
+            this.textResult.TabIndex = 16;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(188, 295);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(61, 25);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Limpa";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 391);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textResult);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textHojeAno);
+            this.Controls.Add(this.textHojeMes);
+            this.Controls.Add(this.textHojeDia);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textAno);
+            this.Controls.Add(this.textMes);
+            this.Controls.Add(this.textDia);
+            this.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textDia;
+        private System.Windows.Forms.TextBox textMes;
+        private System.Windows.Forms.TextBox textAno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textHojeAno;
+        private System.Windows.Forms.TextBox textHojeMes;
+        private System.Windows.Forms.TextBox textHojeDia;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textResult;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ ProjeIdade/ProjeIdade/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjeIdade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int dia, mes, ano, diah, mesh, anoh, result;
+
+            dia = Convert.ToInt32(textDia.Text);
+            mes = Convert.ToInt32(textMes.Text);
+            ano = Convert.ToInt32(textAno.Text);
+            diah = Convert.ToInt32(textHojeDia.Text);
+            mesh = Convert.ToInt32(textHojeMes.Text);
+            anoh = Convert.ToInt32(textHojeAno.Text);
+
+
+            result = ano - anoh;
+
+            if (result <= 17)
+            {
+                result = 0;
+                textResult.Text = result.ToString("Menor De Idade");
+            }
+            else if (result > 18)
+                textResult.Text = result.ToString("Maior De Idade");
+            else
+            {
+                result = 0;
+                result = mes - mesh;
+            }
+            if (result < 0)
+            {
+                result = 0;
+                result = dia - diah;
+            }
+            else
+            {
+                result = 0;
+                textResult.Text = result.ToString("Maior De Idade");
+            }
+            if (result < 0 && result != 0)
+            {
+                result = 0;
+                textResult.Text = result.ToString("Menor De Idade");
+        }
+            else
+                textResult.Text = result.ToString("Feliz Aniversario");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textDia.Text = "";
+            textMes.Text = "";
+            textAno.Text = "";
+            textHojeDia.Text = "";
+            textHojeMes.Text = "";
+            textHojeAno.Text = "";
+            textResult.Text = "";
+        }
+    }
+}
